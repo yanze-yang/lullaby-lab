af9bf60dc0bc181f1c759ae16c8c0825a482daee
Ryan Yang <ryanyang.nz@gmail.com>
2023-01-04 13:20:27 +1300

    button text sumbite to update

--- src/pages/shop/[id].tsx
+++ src/pages/shop/[id].tsx
@@ -106,7 +106,7 @@ const List = (product: IProduct) => {
             type="submit"
             className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
           >
-            Submit
+            Update
           </button>
           <button
             onClick={deleteProduct}

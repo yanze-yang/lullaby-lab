0785c5dcd341cb2be675014b2be55d04af7dd777
Ryan Yang <ryanyang.nz@gmail.com>
2023-01-09 11:49:21 +1300

    prisma contacts model

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -6,10 +6,10 @@ import ProductTable from "../../components/shop/ProductTable";
 import EmptyContent from "../../components/layout/EmptyContent";
 
 export async function getServerSideProps() {
-  const clients = await prisma.client.findMany();
+  const contacts = await prisma.contact.findMany();
 
   // sort by created_at
-  clients.sort((a, b) => {
+  contacts.sort((a, b) => {
     if (a.createdAt < b.createdAt) return 1;
     if (a.createdAt > b.createdAt) return -1;
     return 0;
@@ -17,20 +17,20 @@ export async function getServerSideProps() {
 
   return {
     props: {
-      clients: JSON.parse(JSON.stringify(clients)),
+      contacts: JSON.parse(JSON.stringify(contacts)),
     },
   };
 }
 
-export default function ClientIndex({ clients }: { clients: IClient[] }) {
+export default function ContactIndex({ contacts }: { contacts: IClient[] }) {
   return (
     <div className="h-[100vh] dark:bg-gray-900">
       <Navbar />
-      {clients.length > 0 ? (
+      {contacts.length > 0 ? (
         // <ProductTable clients={clients} />
         <></>
       ) : (
-        <EmptyContent>No client found</EmptyContent>
+        <EmptyContent>No contacts found</EmptyContent>
       )}
     </div>
   );

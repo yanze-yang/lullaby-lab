d7e812e45e6deac771b2af99cae4aa209a79c291
Ryan Yang <ryanyang.nz@gmail.com>
2023-01-08 17:20:39 +1300

    fix default date

--- src/components/form/OrderForm.tsx
+++ src/components/form/OrderForm.tsx
@@ -12,6 +12,7 @@ import { useForm, Controller, NestedValue } from "react-hook-form";
 import ReactSelect from "react-select";
 
 import { InputStyle, InputStyleReadOnly, LabelStyle } from "./FormStyle";
+import moment from "moment";
 
 interface IProductOption {
   value: string;
@@ -61,6 +62,7 @@ const OrderForm = ({ order, products, operation }: Props) => {
   const onSubmit = (data: FormValues) => {
     console.log("data", data);
   };
+  console.log("order.date", order.date);
 
   return (
     <div className="h-[100vh] dark:bg-gray-900">
@@ -107,7 +109,7 @@ const OrderForm = ({ order, products, operation }: Props) => {
                   type="date"
                   className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                   placeholder="Select date"
-                  defaultValue={order?.date?.toString()}
+                  defaultValue={moment(order?.date).format("YYYY-MM-DD")}
                 />
               </div>
               {errors.date && (

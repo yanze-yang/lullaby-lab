c4a30632d0fad5ca649348563000d4c102bfac90
Ryan Yang <ryanyang.nz@gmail.com>
2023-01-08 14:21:58 +1300

    create button to product

--- src/components/button/CreateButton.tsx
+++ src/components/button/CreateButton.tsx
@@ -12,7 +12,7 @@ const CreateButton = () => {
       <Link href={`${path}/create`}>
         <button
           type="button"
-          className="absolute bottom-10 right-10 mr-2 inline-flex items-center rounded-full bg-blue-700 p-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="absolute bottom-16 right-10 mr-2 inline-flex items-center rounded-full bg-blue-700 p-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           <svg
             className="h-6 w-6"

14244e4212f1abe3a086a465a5f8466559aee95c
Ryan Yang <ryanyang.nz@gmail.com>
2023-01-09 21:57:11 +1300

    sign in page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,15 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 const signin = () => {
   return (
-    <div className="h-screen bg-gray-700 bg-no-repeat">
+    <div className="h-screen bg-[#1b2642] ">
+      <img
+        src="/hero-min.png"
+        alt="hero"
+        className="absolute bottom-0 h-auto w-[80%]"
+      />
       <div
         id="authentication-modal"
         tabIndex={-1}
         aria-hidden="true"
         className="fixed top-10 left-0 right-0 z-50 h-modal w-full overflow-y-auto overflow-x-hidden p-4 md:inset-0 md:h-full"
       >
-        <div className="absolute top-[15rem] right-[14rem] h-full w-full max-w-md md:h-auto">
+        <div className="absolute top-[20%] mx-0 w-full max-w-md md:right-[10%] md:h-auto">
           <div className="relative rounded-lg bg-white shadow dark:bg-gray-900 ">
             <button
               type="button"
@@ -26,19 +32,29 @@ const signin = () => {
                 <path
                   fillRule="evenodd"
                   d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               <span className="sr-only">Close modal</span>
             </button>
             <div className="px-6 py-6 lg:px-8">
-              <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
-                Sign in to our platform
+              <h3 className="mb-4 text-xl font-medium uppercase text-gray-900 dark:text-white">
+                Welcome to <b>supa</b>!
               </h3>
+              <div className="mb-5 flex flex-col gap-2 text-sm font-medium text-gray-500 dark:text-gray-300">
+                <div>
+                  Supa is a simple and fast tool for managing orders and
+                  clients.
+                </div>
+                <div>
+                  Log in to access your account, or click "Continue as guest" to
+                  explore the demo data.
+                </div>
+              </div>
               <form className="space-y-6" action="#">
                 <button
                   type="button"
-                  className="flex w-full items-center justify-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="mb-3 flex w-full items-center justify-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   <svg
                     className="mr-2 -ml-1 h-4 w-4"
@@ -57,16 +73,15 @@ const signin = () => {
                   </svg>
                   Sign in with Google
                 </button>
-                <button
-                  type="button"
-                  className="mr-2 mb-2 inline-flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3.5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                >
-                  <div className="mr-2">👋</div>
-                  <div>Continue as Guest</div>
-                </button>
-                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                  Explore the demo data as the guest.
-                </div>
+                <Link href="/order">
+                  <button
+                    type="button"
+                    className="mr-2 mb-2 inline-flex w-full items-center justify-center rounded-lg border border-gray-200 bg-white px-3.5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                  >
+                    <div className="mr-2">👋</div>
+                    <div>Continue as Guest</div>
+                  </button>
+                </Link>
               </form>
             </div>
           </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-pattern":
-          "linear-gradient(90deg, rgba(13,37,65,0.14891894257703087) 33%, rgba(25,50,79,1) 74%, rgba(19,37,58,1) 100%), url('/hero-min.png')",
+        'hero-pattern': "url('/hero-min.png')",
       },
     },
   },

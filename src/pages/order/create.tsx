5e3a0935940798201d6cad022cc2fe508978f367
Ryan Yang <ryanyang.nz@gmail.com>
2023-01-12 16:09:21 +1300

    update

--- src/pages/order/create.tsx
+++ src/pages/order/create.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { prisma } from "../../server/db/client";
-import type { IProduct } from "../../types";
+import type { IContact, IProduct } from "../../types";
 import OrderForm from "../../components/form/OrderForm";
+import { getSession } from "next-auth/react";
 
 export async function getStaticProps() {
-  const products = await prisma.product.findMany();
+  const session = await getSession();
+  const products = await prisma.product.findMany({
+    where: { userId: session?.user?.id },
+  });
+
+  const contacts = await prisma.contact.findMany({
+    where: { userId: session?.user?.id },
+    select: { id: true, name: true },
+  });
 
   if (products) {
     return {
       props: {
         products: JSON.parse(JSON.stringify(products)),
+        contacts: JSON.parse(JSON.stringify(contacts)),
       },
     };
   }
@@ -22,8 +32,16 @@ export async function getStaticProps() {
   };
 }
 
-const CreateOrder = ({ products }: { products: IProduct[] }) => {
-  return <OrderForm products={products} operation="create" />;
+const CreateOrder = ({
+  products,
+  contacts,
+}: {
+  products: IProduct[];
+  contacts: IContact[];
+}) => {
+  return (
+    <OrderForm products={products} contacts={contacts} operation="create" />
+  );
 };
 
 export default CreateOrder;
